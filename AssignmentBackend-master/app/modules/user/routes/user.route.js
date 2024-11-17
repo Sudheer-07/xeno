5f111082d71ad0a0c982f84972ad6d38247720ba
Sudheer <90494976+Sudheer-07@users.noreply.github.com>
2024-11-17 12:48:32 +0530

    Delete AssignmentBackend-master directory

--- AssignmentBackend-master/app/modules/user/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { login} = require('../controllers/user.controller');
-
-router.post('/auth/google', login);
-
-
-module.exports = router;
